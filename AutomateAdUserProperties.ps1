b5268924c6feb15f984d55615b7243d2bbf431c1
Linus Gates <45574577+nikolazleo@users.noreply.github.com>
2024-03-13 16:48:58 -0500

    edited out security changes

--- AutomateAdUserProperties.ps1
+++ AutomateAdUserProperties.ps1
@@ -3,29 +3,29 @@ param (
     [string]$FileName
 )
 
-Try {
-    # Import the custom module
-    Import-Module $("{0}{1}"-f $Path, "module.psm1") -Force
-}
-catch {
-    Write-Host "Error: $_"
-}
+# Try {
+#     # Import the custom module
+#     Import-Module $("{0}{1}"-f $Path, "module.psm1") -Force
+# }
+# catch {
+#     Write-Host "Error: $_"
+# }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls1
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls1
+# [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
     
-    $ignoreCerts = @"
-public class SSLHandler
-{
-    public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
-    {
-        return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
-    }
-}
-"@
+#     $ignoreCerts = @"
+# public class SSLHandler
+# {
+#     public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
+#     {
+#         return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+#     }
+# }
+# "@
 
-Add-Type -TypeDefinition $ignoreCerts
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
+# Add-Type -TypeDefinition $ignoreCerts
+# [System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
 
  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Confirm:$false 
 
