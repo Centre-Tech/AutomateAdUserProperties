15a44215bca33f679cfe02266d010c686f909e38
Linus Gates <45574577+nikolazleo@users.noreply.github.com>
2024-03-13 13:45:29 -0500

    Added Sec protocol settings

--- AutomateAdUserProperties.ps1
+++ AutomateAdUserProperties.ps1
@@ -11,6 +11,24 @@ catch {
     exit 1
 }
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls1
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
+    
+    $ignoreCerts = @"
+public class SSLHandler
+{
+    public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
+    {
+        return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+    }
+}
+"@
+
+Add-Type -TypeDefinition $ignoreCerts
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
+
+ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Confirm:$false 
+
 # Call the function and specify the path and file name parameters
 $adUsersPath = Get-AdUsers -Path $Path -FileName $FileName
 
